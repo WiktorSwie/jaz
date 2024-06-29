8bd94fb55a1efc2832dea9f5ac212c1c2beef9c9
WiktorSwie <115349384+WiktorSwie@users.noreply.github.com>
2024-06-29 11:17:54 +0200

    Add files via upload

---
+++ srednia_waluta.sql
@@ -0,0 +1,13 @@
+create table srednia_waluta
+(
+    id                  int auto_increment
+        primary key,
+    godzina_zap         time         not null,
+    waluta              varchar(255) not null,
+    data_rozpoczynajaca date         not null,
+    data_koncowa        date         not null,
+    sredni_kurs         double       not null,
+    data_zapytania      date         not null
+);
+
+
