e787c9b5e04a2297bf8396fe4568d643a6cc3294
WiktorSwie <115349384+WiktorSwie@users.noreply.github.com>
2024-06-29 10:33:50 +0200

    Add files via upload

---
+++ Waluty.sql
@@ -0,0 +1,13 @@
+create table Waluty
+(
+    id                  int auto_increment
+        primary key,
+    godzina_zap         time   not null,
+    waluta              int    not null,
+    data_rozpoczynajaca date   not null,
+    data_koncowa        date   not null,
+    sredni_kurs         double not null,
+    data_zapytania      date   not null
+);
+
+
